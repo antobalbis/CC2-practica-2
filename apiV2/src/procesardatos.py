6056d0c6b9c4a966a6ce0b71e9027ee7fc4c20c8
Antonio Balbís <antoniobalh@gmail.com>
2021-05-19 12:02:55 +0200

    creación de la versión 2 del servicio

---
+++ apiV2/src/procesardatos.py
@@ -0,0 +1,53 @@
+from statsmodels.tsa.ar_model import AutoReg
+from pymongo import MongoClient
+from bson.json_util import dumps, loads
+from flask import Flask
+import pandas as pd
+import numpy as np
+import datetime
+
+server = Flask(__name__)
+
+def predict(n_periods):
+    client = MongoClient('mongodb://%s:%s@mongodb:27017' % ('admin', 'password'))
+    db = client.datos_tiempo
+    col = db.tiempo
+    datos = dumps(col.find_one())
+
+    df = pd.read_json(datos)
+    df = df.iloc[1:40]
+
+    model = AutoReg(df.sanfranciscohumidity, lags = 1)
+    model2 = AutoReg(df.sanfranciscotemperature, lags = 1)
+
+    model_fit = model.fit()
+    model2_fit = model2.fit()
+
+# Forecast
+    fc = model.predict(np.ndarray(shape = (2, 1), dtype = float), start = 0, end = n_periods)
+    fc2 = model2.predict(np.ndarray(shape = (2, 1), dtype = float), start = 0, end = n_periods)
+
+    print(fc)
+    print(fc2)
+    json_ = '{"predicciones": }'
+
+    for x in range(n_periods):
+        json_ = json_ + '{"hour":' + str(datetime.time(x%24,0)) + '"temp": ' + str(fc2[x+1]) + ', "hum": ' + str(fc[x+1]) + '},'
+
+
+    return json_
+
+@server.route("/servicio/v1/prediccion/24horas")
+def prediccion24():
+    return predict(24)
+
+@server.route("/servicio/v1/prediccion/48horas")
+def prediccion48():
+    return predict(48)
+
+@server.route("/servicio/v1/prediccion/72horas")
+def prediccion72():
+    return predict(72)
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0')
